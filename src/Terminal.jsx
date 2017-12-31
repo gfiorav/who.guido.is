81bf0919099f80f017a0d091031f2c50705ec0ad
Guido Fioravantti <guido.fioravantti@gmail.com>
2017-12-31 17:39:13 +0100

    Add TextArea component to Terminal

--- src/Terminal.jsx
+++ src/Terminal.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './Terminal.css';
 
 import TitleBar from './terminal/TitleBar';
+import TextArea from './terminal/TextArea';
 
 const Terminal = () => (
   <div className="terminal">
     <TitleBar />
+    <TextArea />
   </div>
 );
 
