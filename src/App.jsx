9ff1bd842f23fdd7fc10df0a46246fc8902abb7f
Guido Fioravantti <guido.fioravantti@gmail.com>
2017-12-31 14:43:19 +0100

    Initial v2 commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Terminal from './Terminal';
+
+const App = () => (<Terminal />);
+
+export default App;
+
---
+++ src/Terminal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Terminal.css';
+
+const Terminal = () => (<div className="terminal" />);
+
+export default Terminal;
+
