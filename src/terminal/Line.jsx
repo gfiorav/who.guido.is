f679c405e633568014c6fe48a0e3d3852fc6ec80
Guido Fioravantti <guido.fioravantti@gmail.com>
2018-01-02 21:24:29 +0100

    Add Line component

---
+++ src/terminal/Line.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Prompt from './Prompt';
+import Input from './Input';
+
+const Line = () => (
+  <div className="line">
+    <Prompt />
+    <Input />
+  </div>
+);
+
+export default Line;
+
